386b805d3dd153fb1b035a473184f046152ad160
SilentNauscopy <babartosz.bns@gmail.com>
2019-03-01 21:18:19 +0100

    Zad 3 from Lab1 is done. Works for negative values, and uses some improvements.

--- lab1/zad3/src/GreatestDivisor.scala
+++ lab1/zad3/src/GreatestDivisor.scala
@@ -1,19 +1,36 @@
-object GreatestDivisor {
+/**
+  * @author Bartosz Banasik
+  *
+  * Task 3, Lab 1
+  *
+  */
 
+object GreatestDivisor {
+  /**
+    * Find the smallest divisor, after that divide number
+    * by this divisor.
+    *
+    * @param number positive integer
+    * @return greatest divisor for the ''number''
+    */
   def gd(number: Int): Int = {
-    for ( i <- (1 until number).reverse
-          if number % i == 0) return i
+    /** Next line is ugly, but it's just for the exception */
+    if (number == 0) return number/0
+    if (number == 1) return 1
+    for ( i <- 2 to scala.math.sqrt(number).toInt
+          if number % i == 0) return number/i
     1
   }
 
   def main(args: Array[String]): Unit = {
-    args.foreach( i =>
+    args.foreach( f = i =>
       try {
-        val greatest_divisor: Int = gd(i.toInt)
-        println(s"Greatest divisor for ${i.toInt} except ${i.toInt} is: $greatest_divisor")
+        /** Absolute in case of negative values */
+        val greatest_divisor: Int = gd(math.abs(i.toInt))
+        println(s"${i.toInt} : $greatest_divisor")
       } catch {
         case _: NumberFormatException => println("Could not convert '" + i + "' to int.");
-        case ex: Throwable => println("Some throwable dunno, what happened but we keep going..." + ex.printStackTrace)
+        case ex: Throwable => println(i + ": Some throwable dunno, what happened but we keep going..." + ex.printStackTrace)
       }
     )
   }
