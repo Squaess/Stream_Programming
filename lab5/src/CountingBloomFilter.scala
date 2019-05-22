eb9c76378bee14854c1018146985988b03dbb546
SilentNauscopy <babartosz.bns@gmail.com>
2019-05-22 19:29:04 +0200

    Working on lab5

--- lab5/src/CountingBloomFilter.scala
+++ lab5/src/CountingBloomFilter.scala
@@ -1,10 +1,35 @@
 import scala.util.hashing.MurmurHash3
 
-class CountingBloomFilter {
-
+class CountingBloomFilter(a_size:Int, h_number:Int) {
+  require(a_size > 0 && h_number >0)
+  val filter:Array[Int] = new Array[Int](a_size)
   val h1: MurmurHash3.type = MurmurHash3
-  println("Hash1 on 'dupa' " + h1.stringHash("dupa", 1))
-  println("Hash2 on 'dupa' "+ h1.stringHash("dupa", 2))
-  println("Hash1 on 'dupa' " + h1.stringHash("dupa", 1))
+
+  def add(item:Any): Unit = {
+    for(i <- 0 until h_number) {
+      val has = h1.stringHash(item.toString, i) % a_size
+      val idx = if (has >= 0) has else has + a_size
+      filter(idx) += 1
+    }
+  }
+
+  def contains(item:Any): Boolean = {
+    for(i <- 0 until h_number) {
+      val has = h1.stringHash(item.toString, i) % a_size
+      val idx = if (has >= 0) has else has + a_size
+      if (filter(idx) == 0) return false
+    }
+    true
+  }
+
+  def remove(item:Any): Unit = {
+    for(i <- 0 until h_number) {
+      val has = h1.stringHash(item.toString, i) % a_size
+      val idx = if (has >= 0) has else has + a_size
+      filter(idx) = if (filter(idx) > 0) filter(idx) - 1 else throw new NoSuchElementException()
+    }
+  }
+
+  override def toString: String = filter.mkString("")
 
 }
--- lab5/src/Test.scala
+++ lab5/src/Test.scala
@@ -1,5 +1,17 @@
 object Test {
   def main(args: Array[String]): Unit = {
-    val bf = new CountingBloomFilter
+    val bf = new CountingBloomFilter(10,3)
+
+    def naturals(n: Int): Stream[Int] = n #:: naturals(n + 1)
+    (naturals(0) take 2) foreach bf.add
+    println(bf)
+
+    (naturals(0) take 2) foreach bf.add
+    println(bf)
+    println(bf.contains(2))
+    bf.add(2)
+    println(bf.contains(2))
+    bf.remove(2)
+    println(bf.contains(2))
   }
 }
