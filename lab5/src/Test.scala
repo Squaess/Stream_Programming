390038cd1855d4da14b35ff5c25f9510ec991d49
SilentNauscopy <babartosz.bns@gmail.com>
2019-05-28 00:48:39 +0200

    Lab5 done. Need to submit solution

--- lab5/src/Test.scala
+++ lab5/src/Test.scala
@@ -3,15 +3,17 @@ object Test {
     val bf = new CountingBloomFilter(10,3)
 
     def naturals(n: Int): Stream[Int] = n #:: naturals(n + 1)
-    (naturals(0) take 2) foreach bf.add
+    (naturals(0) take 3) foreach bf.add
     println(bf)
 
-    (naturals(0) take 2) foreach bf.add
-    println(bf)
-    println(bf.contains(2))
-    bf.add(2)
-    println(bf.contains(2))
     bf.remove(2)
+    println(bf)
+
+
+    println(bf.contains(2))
+    println(bf.contains(0))
+    println(bf.contains(-1))
+    bf.add(2)
     println(bf.contains(2))
   }
 }
